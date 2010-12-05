dcbc173e01dcfd3bbfc39d953c527e84aa0cc020
Chris Soyars <ctso@ctso.me>
2010-12-05 07:02:38 -0500

    Fix supersonic.
    Change-Id: Icf11083b4f4a8f4437faec05b75bedbaba77dbe1

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,4 +1,5 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/mau5_passion.mk \
     $(LOCAL_DIR)/mau5_vision.mk \
-    $(LOCAL_DIR)/mau5_sholes.mk
+    $(LOCAL_DIR)/mau5_sholes.mk \
+    $(LOCAL_DIR)/mau5_supersonic.mk
