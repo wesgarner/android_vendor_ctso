0711862b936bdddd9ccd05f74cbe58b5e22e167b
Chris Soyars <ctso@ctso.me>
2010-12-05 02:19:53 -0500

    Add default ringtone.

--- products/mau5.mk
+++ products/mau5.mk
@@ -8,6 +8,10 @@ PRODUCT_NAME := mau5_common
 PRODUCT_PACKAGE_OVERLAYS := vendor/ctso/overlay/common
 PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/common
 
+# Set Default Ringtone
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.ringtone=SometimesThingsGet.ogg
+
 PRODUCT_PROPERTY_OVERRIDES += \
     rild.libpath=/system/lib/librilswitch.so \
     rilswitch.vendorlibpath=/system/lib/libhtc_ril.so \
@@ -22,6 +26,7 @@ PRODUCT_COPY_FILES += \
     vendor/ctso/prebuilt/common/fonts/DroidSans.ttf:system/fonts/DroidSans.ttf \
     vendor/ctso/prebuilt/common/fonts/DroidSans-Bold.ttf:system/fonts/DroidSans-Bold.ttf \
     vendor/ctso/prebuilt/common/bin/setdefcols.sh:system/bin/setdefcols.sh \
-    vendor/ctso/prebuilt/common/app/ThemeDefaults.apk:system/app/ThemeDefaults.apk
+    vendor/ctso/prebuilt/common/app/ThemeDefaults.apk:system/app/ThemeDefaults.apk \
+    vendor/ctso/prebuilt/common/sounds/SometimesThingsGet.ogg:system/media/audio/ringtones/SometimesThingsGet.ogg
 
 PRODUCT_PACKAGES += Mau5Wallpapers
