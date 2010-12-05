d7266a4f532de01a21ade70f046034e891e71c13
Chris Soyars <ctso@ctso.me>
2010-12-05 02:49:51 -0500

    get rid of common overlay

--- products/mau5_vision.mk
+++
@@ -1,9 +0,0 @@
-# Inherit cyanogenmod configurations
-$(call inherit-product, vendor/cyanogen/products/cyanogen_vision.mk)
-$(call inherit-product, vendor/ctso/products/mau5.mk)
-
-PRODUCT_NAME := mau5_passion
-
-# Reorder Product Overlays to properly include theme specific stuff before
-# anything else
-PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/vision
