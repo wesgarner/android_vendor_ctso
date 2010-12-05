5bc20b4986282a0168311430a887c0c42d0e69d5
Chris Soyars <ctso@ctso.me>
2010-12-05 16:15:26 -0500

    Get rid of common.
    Change-Id: I6237dc36e8f5f2ddc98bef296a7720cc09630789

--- products/mau5_supersonic.mk
+++ products/mau5_supersonic.mk
@@ -1,5 +1,22 @@
 # Inherit cyanogenmod configurations
 $(call inherit-product, vendor/cyanogen/products/cyanogen_supersonic.mk)
-$(call inherit-product, vendor/ctso/products/mau5_common.mk)
 
 PRODUCT_NAME := mau5_supersonic
+
+## common stuff ##
+PRODUCT_PACKAGE_OVERLAYS := vendor/ctso/overlay/common
+PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/common
+
+# Set Default Ringtone
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.ringtone=SometimesThingsGet.ogg
+
+PRODUCT_COPY_FILES += \
+    vendor/ctso/prebuilt/common/fonts/DroidSans.ttf:system/fonts/DroidSans.ttf \
+    vendor/ctso/prebuilt/common/fonts/DroidSans-Bold.ttf:system/fonts/DroidSans-Bold.ttf \
+    vendor/ctso/prebuilt/common/bin/setdefcols.sh:system/bin/setdefcols.sh \
+    vendor/ctso/prebuilt/common/sounds/SometimesThingsGet.ogg:system/media/audio/ringtones/SometimesThingsGet.ogg
+
+PRODUCT_PACKAGES += Mau5Wallpapers
+PRODUCT_PACKAGES += ThemeDefaults
+
