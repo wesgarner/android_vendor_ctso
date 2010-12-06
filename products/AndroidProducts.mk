57dbcb255d121e829f5d09883ce3032d9e42330d
Chris Soyars <ctso@ctso.me>
2010-12-05 21:41:11 -0500

    Add bravoc.
    Change-Id: Iec4de2a794fd8c05f56aac6b1db5f440f942fc6d

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -2,4 +2,5 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/mau5_passion.mk \
     $(LOCAL_DIR)/mau5_vision.mk \
     $(LOCAL_DIR)/mau5_sholes.mk \
-    $(LOCAL_DIR)/mau5_supersonic.mk
+    $(LOCAL_DIR)/mau5_supersonic.mk \
+    $(LOCAL_DIR)/mau5_bravoc.mk
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,3 +2,4 @@ add_lunch_combo mau5_passion-eng
 add_lunch_combo mau5_vision-eng
 add_lunch_combo mau5_sholes-eng
 add_lunch_combo mau5_supersonic-eng
+add_lunch_combo mau5_bravoc-eng
