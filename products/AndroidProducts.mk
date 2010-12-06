2662746c52ecf803a2d744e3dac400fede207fea
Wes Garner <wesgarner@gmail.com>
2010-12-05 22:21:26 -0600

    mau5: Add MSM7227 series devices (espresso, liberty, legend)

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -3,4 +3,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/mau5_vision.mk \
     $(LOCAL_DIR)/mau5_sholes.mk \
     $(LOCAL_DIR)/mau5_supersonic.mk \
-    $(LOCAL_DIR)/mau5_bravoc.mk
+    $(LOCAL_DIR)/mau5_bravoc.mk \
+    $(LOCAL_DIR)/mau5_espresso.mk \
+    $(LOCAL_DIR)/mau5_liberty.mk \
+    $(LOCAL_DIR)/mau5_legend.mk
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,3 +3,6 @@ add_lunch_combo mau5_vision-eng
 add_lunch_combo mau5_sholes-eng
 add_lunch_combo mau5_supersonic-eng
 add_lunch_combo mau5_bravoc-eng
+add_lunch_combo mau5_espresso-eng
+add_lunch_combo mau5_liberty-eng
+add_lunch_combo mau5_legend-eng
